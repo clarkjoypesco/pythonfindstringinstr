d3f1908d732adb1437845b2ecb96bff33d46e794
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-08 19:43:49 +0800

    initial commit

---
+++ findstringinstr.py
@@ -0,0 +1,9 @@
+einstein = 'There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.'
+
+print einstein.find('miracle')
+
+print einstein[73:]
+
+print einstein.find('two')
+
+print einstein[15:]
